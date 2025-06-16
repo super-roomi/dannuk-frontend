8480236f38466af395a5a49aa2cbc7c72fc01007
_roomi <roomiisdead@gmail.com>
2025-06-16 17:38:56 +0300

    finalized navbar and homepage placeholder

--- dannuk/src/components/Navbar.jsx
+++ dannuk/src/components/Navbar.jsx
@@ -6,11 +6,11 @@ function Navbar() {
     //const [menuOpen, setMenuOpen] = useState(false);
 
     return (
-        <nav className="bg-teal-100 p-4 fixed w-full">
-            <div className="flex flex-col items-center md:flex-row md:justify-between w-full">
+        <nav className="flex mt-2 p-1 w-full">
+            < div className="flex flex-col items-center md:flex-row md:justify-between w-full" >
                 {/* Logo & Text */}
-                <div className='flex gap-x-5'>
-                    <div className="flex flex-col items-center md:flex-row md:items-center md:gap-4">
+                <div div className='flex gap-x-5'>
+                    <div className="flex flex-col items-center md:flex-row md:items-center md:gap-1">
                         <img src="/logo.png" alt="Dannuk Logo" className="w-24 h-24 lg:w-20 lg:h-20 p-0.5" />
                         <h1 className="text-teal-500 text-3xl mt-2 md:mt-0">Dannuk</h1>
                     </div>
@@ -41,19 +41,19 @@ function Navbar() {
                                 hash: "#hash",
                             }}>Jobs</Link>
                     </div>
-                </div>
+                </div >
 
                 {/* Login - hidden on mobile */}
-                <div className="hidden md:flex items-center gap-x-2 mt-4 md:mt-0">
-                    <button className="underline p-1">Login</button>
-                </div>
+                < div className="hidden md:flex items-center gap-x-2 mt-4 md:mt-0 mr-3" >
+                    <button className="underline p-1 border rounded-full px-4 backdrop-blur-2xl">Login</button>
+                </ div>
                 {/* Burgah icon */}
                 {/* <div className="md:hidden">
                     <button onClick={() => setMenuOpen(!menuOpen)} className="text-teal-800">
                         {menuOpen ? <X className="w-6 h-6" /> : <Menu className="w-6 h-6" />}
                     </button>
                 </div> */}
-            </div>
+            </div >
 
             {/* burgah menu */}
             {/* {menuOpen && (
@@ -65,7 +65,7 @@ function Navbar() {
                     <button className="underline">Login</button>
                 </div>
             )} */}
-        </nav>
+        </nav >
     )
 }
 
--- dannuk/src/pages/Home.jsx
+++ dannuk/src/pages/Home.jsx
@@ -4,8 +4,23 @@ import Navbar from '../components/Navbar'
 function Home() {
     return (
         <>
-            <Navbar />
-            <div className=''></div>
+            <div className='min-h-screen flex flex-col'>
+                <Navbar />
+
+                {/* spinny logo woooo */}
+                {/* <div className='relative overflow-hidden h-svh'>
+                    <div className='absolute -right-64 -bottom-100 lg:-right-86 lg:bottom-0 h-svh z-0'>
+                        <img src="/logo.png" alt="" className='md:h-svh animate-spin duration-500 ' style={{ animationDuration: '30s' }} />
+                    </div>
+                </div> */}
+
+
+                <div className='flex flex-col content-center items-center p-4 flex-1 lg:mt-10 md:mt-10 sm: mt-2'>
+                    <h1 className='lg:text-8xl text-7xl text-center'>The <span className='text-teal-500'>one stop</span> for all your <span className='text-teal-500'>needs.</span></h1>
+                    <h1 className="lg:text-6xl text-4xl">More suff here.</h1>
+                </div>
+            </div>
+
         </>
     )
 }
